ac301d6247569cdc8115b82fd48e99c7eee53d9c
Oscar San <oscarsani007@gmail.com>
2025-08-01 20:21:03 -0400

    Poisons Puddle Added, New Tag for Player, Sorting Layer Adjustments

---
+++ GMTK-2025/Assets/Scripts/Spells/PoisonPuddle.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PoisonPuddle : Spell
+{
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Init(); // Initialize the spell properties
+        Destroy(gameObject, destroyTime); // Destroy the spell after a certain time
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            SimpleEnemy enemy = collision.GetComponent<SimpleEnemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(GetDamage());
+            }
+        }
+    }
+}
