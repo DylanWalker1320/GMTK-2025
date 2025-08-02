d6a9e35cb7b8d5bdb6c62866db13eb36a557b47f
Dylan Walker <dylan.walker1320@gmail.com>
2025-08-02 14:59:26 -0400

    Steam Vent balancing

--- GMTK-2025/Assets/Scripts/Spells/SteamVent.cs
+++ GMTK-2025/Assets/Scripts/Spells/SteamVent.cs
@@ -14,7 +14,7 @@ public class SteamVent : Spell
         OrientSpell(); // Ensure the spell is pointing toward the reticle
 
         transform.Rotate(0, 0, 90f);
-        transform.localScale = new Vector3(range * 0.8f, range, 1f); // Scale the spell to the desired range
+        transform.localScale = new Vector3(range * 0.6f, range, 1f); // Scale the spell to the desired range
         transform.position += transform.up * -range / 2; // Adjust position to avoid immediate collision
 
         Destroy(gameObject, destroyTime);
