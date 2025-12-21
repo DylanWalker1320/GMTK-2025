76ae0e699bd5d7e2bda6c0ca2076bdef7307635c
Dylan Walker <dylan.walker1320@gmail.com>
2025-12-21 01:03:35 -0500

    Revert "Merge branch 'feature-saving' into development"
    This reverts commit cc588444e21d710d3f6a82b1058e22b2a4d5ef72, reversing
    changes made to 968a5c2faedce6c4fbcb16ba2bb41dcdf7e180c4.

--- GMTK-2025/Assets/Scripts/Hat.cs
+++ GMTK-2025/Assets/Scripts/Hat.cs
@@ -1,12 +1,35 @@
 using UnityEngine;
+using System.Collections;
 
 public class Hat : MonoBehaviour
 {
-    public GeneratedHat hatData;
-    
-    // Called by HatGenerator after instantiation
-    public virtual void Initialize(GeneratedHat data, bool applyStats = true)
+    public GameObject hatBelow;
+    private static readonly float moveThreshold = 0.01f;
+
+    void Update ()
     {
-        hatData = data;
+        // If this is the first hat, rigidly position it above the player.
+        if (hatBelow.CompareTag("Player"))
+        {
+            transform.position = hatBelow.transform.position + new Vector3(0, 1.5f, 0); 
+        }
+
+        // Otherwise, try and lerp to the hat below it.
+        if (Vector3.Distance(transform.position, hatBelow.transform.position + new Vector3(0, 0.25f, 0)) > moveThreshold)
+        {
+            // Clamp the y position to be above the hat below
+            transform.position = new Vector3(transform.position.x, hatBelow.transform.position.y + 0.25f, transform.position.z);
+            StartCoroutine(MoveHat());
+        }
     }
+
+    IEnumerator MoveHat()
+    {
+        if (hatBelow != null)
+        {
+            Vector3 targetPosition = hatBelow.transform.position + new Vector3(0, 0.25f, 0);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 10f);
+        }
+        yield return null;
+    }   
 }
