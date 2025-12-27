bc2cdcb38e65f5c6528e3e66fd344c4c7cb7efba
Dylan Walker <dylan.walker1320@gmail.com>
2025-12-27 16:34:12 -0500

    significantly better dialogue code

--- GMTK-2025/Assets/Scripts/DialogueManager.cs
+++ GMTK-2025/Assets/Scripts/DialogueManager.cs
@@ -8,125 +8,81 @@ public class DialogueManager : MonoBehaviour
     [SerializeField] private float textSpeed = 0.05f;
     [SerializeField] private TextMeshProUGUI dialogueText;
     [SerializeField] private Image dialogueBoxImage;
+    [SerializeField] private bool debugMode = false;
+    private bool dialogueMode = false;
+    private int currentLineIndex = 0;
+    private List<string> activeDialogueLines;
    
     // You'd normally load this from a json / txt file using Ink for dialogue trees, but we only need a few lines so...
-    private List<string> newGame1 = new List<string>
+    public static List<string> newGame1 = new List<string>
     {
         "... Where am I?",
         "...",
         "My hat! I must find my hat.",
     };
-    private List<string> newGame2 = new List<string>
+    public static List<string> newGame2 = new List<string>
     {
         "This isn't my hat...",
         "It is now though!",
         "... Now where is my real hat?",
     };
-    private List<string> newLoop1 = new List<string>
+    public static List<string> newLoop1 = new List<string>
     {
         "... Here again!?",
         "...",
         "My hats!",
     };
-    private List<string> newLoop2 = new List<string>
+    public static List<string> newLoop2 = new List<string>
     {
         "I still need to find my hat...",
     };
 
-    private bool isTyping = false;
-    private int index = 0;
-    private List<string> dialogueLines = new List<string>();
-
-    public enum DialogueType
+    void StartDialogue(List<string> dialogueLines)
     {
-        NewGame1,
-        NewGame2,
-        NewLoop1,
-        NewLoop2
+        dialogueBoxImage.enabled = true;
+        dialogueMode = true;
+        activeDialogueLines = dialogueLines;
+        currentLineIndex = 0;
+        StartCoroutine(TypeDialogue(activeDialogueLines[currentLineIndex]));
     }
 
     void Update()
     {
-        if (isTyping)
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
-            dialogueBoxImage.enabled = true;
-        }
-        else
-        {
-            dialogueBoxImage.enabled = false;
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (!isTyping)
-            {
-                Debug.Log("Proceeding to next line.");
-                NextLine();
-            }
-            else
-            {
-                Debug.Log("Skipping typing animation.");
-                StopAllCoroutines();
-                dialogueText.text = dialogueLines[index];
+            if (dialogueMode)
+            {   // Advance dialogue
+                currentLineIndex++;
+                if (currentLineIndex < activeDialogueLines.Count)
+                {
+                    StopAllCoroutines();
+                    dialogueText.text = activeDialogueLines[currentLineIndex];
+                    StartCoroutine(TypeDialogue(activeDialogueLines[currentLineIndex]));
+                } // End of dialogue
+                else
+                {
+                    dialogueBoxImage.enabled = false;
+                    dialogueMode = false;
+                    currentLineIndex = 0;
+                    dialogueText.text = "";
+                }
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.P))
+        if (debugMode && Input.GetKeyDown(KeyCode.P))
         {
-            StartDialogue(DialogueType.NewGame1);
+            // For testing purposes only
+            StartDialogue(newGame1);
         }
-    }
-
-    List<string> PickDialogue(DialogueType type)
-    {
-        return type switch
-        {
-            DialogueType.NewGame1 => newGame1,
-            DialogueType.NewGame2 => newGame2,
-            DialogueType.NewLoop1 => newLoop1,
-            DialogueType.NewLoop2 => newLoop2,
-        };
-    }
-
-    void StartDialogue(DialogueType type)
-    {
-        Debug.Log("Starting conversation.");
-        isTyping = true;
-        dialogueText.text = string.Empty;
-        dialogueLines = PickDialogue(type);
-
-        StartCoroutine(TypeDialogue(dialogueLines[index]));
     }
 
     IEnumerator TypeDialogue(string line)
     {
-        foreach (char c in line.ToCharArray())
+        dialogueText.text = "";
+        foreach (char letter in line.ToCharArray())
         {
-            dialogueText.text += c;
+            dialogueText.text += letter;
             yield return new WaitForSeconds(textSpeed);
         }
-    }
-
-    void NextLine()
-    {
-        if (index < dialogueLines.Count)
-        {
-            index++;
-            dialogueText.text = string.Empty;
-            StartCoroutine(TypeDialogue(dialogueLines[index]));
-        }
-        else
-        {
-            EndDialogue();
-        }
-    }
-
-    void EndDialogue()
-    {
-        Debug.Log("End of conversation.");
-        isTyping = false;
-        index = 0;
-        dialogueText.text = string.Empty;
-        gameObject.SetActive(false);
     }
 }
